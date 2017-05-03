97beaef32164b6c75caf5c944ffabc11d9e75558
pablocarbonell <pabloraulcabonell@gmail.com>
2017-05-03 10:07:24 -0300

    creado el proyecto

---
+++ app/src/main/java/pablocarbonell/tp_progav/MainActivity.java
@@ -0,0 +1,13 @@
+package pablocarbonell.tp_progav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
